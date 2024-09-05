60ae73ef2ac32f2577e34a5238a37a31674b26b1
ushna parvaiz <ushnapervaiz10@gmail.com>
2024-09-05 11:58:23 +0500

    Add week3/ to practice branch

---
+++ week3/task2a.js
@@ -0,0 +1,14 @@
+// Define functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Export multiple functions
+  module.exports = {
+    add,
+    subtract
+  };
---
+++ week3/task2b.js
@@ -0,0 +1,6 @@
+// Import the functions from math.js
+const math = require('./task2a');
+
+// Use the imported functions
+console.log(math.add(3, 5));        // Output: 8
+console.log(math.subtract(10, 4));  // Output: 6
