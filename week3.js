9ce77922e2956ee9b6480cf9f8967c45d439773b
ushna parvaiz <ushnapervaiz10@gmail.com>
2024-09-05 11:37:53 +0500

    Add week3.js to practice branch

---
+++ week3.js
@@ -0,0 +1,38 @@
+function arrayOfObj(){
+    const arr=[];
+
+    //create obj
+    const car1={
+        namee: "audi",
+        colour:"red",
+        price:12
+
+    };
+
+
+    const car2={
+        namee:"hounda",
+        colour:"black",
+        price:3
+
+    };
+
+    const car3={
+        namee:"civic",
+        colour:"white",
+        price:2
+    };
+
+    //add objs to the array
+    arr.push(car1);
+    arr.push(car2);
+    arr.push(car3);
+
+    //return
+    return arr;
+
+}
+//call
+const result=arrayOfObj();
+//console
+console.log(result);
