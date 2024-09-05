c52806b73d85a44d00d4add35260b7123b3d64bf
ushna parvaiz <ushnapervaiz10@gmail.com>
2024-09-05 12:08:49 +0500

    Resolved merge conflicts

--- week2.js
+++ week2.js
@@ -1,1 +1,56 @@
-console.log("hello world");
+console.log("hello world"); 
+//practice tasks
+// //TASK #1
+// //example #1
+//normal
+function myName(){
+    var name="ushna parvaiz";
+   
+console.log( name)
+}
+//anonymous
+const myAge=function(){
+   var age="20";
+console.log(age)
+}
+//arrow functiom
+const myCGPA=()=>{
+   var Cgpa="4";
+   console.log(Cgpa)
+}
+myName();
+myAge();
+myCGPA();
+
+//example#2
+
+
+//example#3
+//ternary operators
+if(2<3)
+{
+   console.log( "2 is less than 3");
+
+}   else{
+   console.log("error");
+} 
+//ternary if
+(2<3)?console.log ('2 is less than 3'): console.log ("error");
+
+//with AND op
+(2<3)&& console.log("2 is less than 3")
+
+//Array type 1
+const car =[];
+car[0] = 'Audi'
+car[1] = 'BMW'
+car[2] = 'BUS CARO'
+console.log(car)
+
+//Array type 2
+const BusCaro=['civic','mehran'];
+console.log(BusCaro)
+
+//Array type 3
+const cars=new Array('BMWW','bike')
+console.log(cars);
